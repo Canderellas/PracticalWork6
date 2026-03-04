afc21ee21d6150c4872ba597ec419c436367b071
Canderellas <ibraevtemurr@gmail.com>
2026-03-04 15:55:03 +0700

    Add ViewModel for MainWindow

---
+++ Model/PartnerView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    class PartnerView
+    {
+        public PartnerView(Partner partner, int skidka)
+        {
+            this.partner = partner;
+            this.skidka = skidka;
+        }
+        public Partner partner { get; set; }
+
+        public int skidka { get; set; }
+    }
+
+}
+
